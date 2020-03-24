2fb99d4fd4148293a04020c90b4a1bf611e65a3f
Joshua McKinney <jmckinney0040@gmail.com>
2020-03-24 00:07:21 -0600

    listed the Bonuses

--- fix.js
+++ fix.js
@@ -1,15 +1,6 @@
-var data = require("./cards.json");
-suitNames = ["Weapon","Beast","Land","Flood","Wizard","Artifact","Leader","Army","Flame","Weather","Wild",]
-count = 0;
-data = data.reduce((acc , suit, suitI) => acc.concat(suit.Cards.map((card, i) => {
-    return {
-        "Suit": suitNames[suitI],
-        "Name": card.Name,
-        "Value": card.Value,
-        "Action": card.Action,
-        "Penalty": card.Penalty,
-        "Bonus": card.Bonus,
-    }; 
-})), []);
+var cards = require("./cards.json");
 
-console.log(JSON.stringify(data,null,4));
+cards = cards.filter(card => card.Bonus !== undefined);
+
+console.log(JSON.stringify(cards,null,4));
+console.log(cards.length);
